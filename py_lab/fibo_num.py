14c478dc771ea48774f25ad641800d6f2042bf63
jupiter6676 <62585191+jupiter6676@users.noreply.github.com>
2020-04-08 18:25:54 +0900

    Update fibo_num.py

--- py_lab/fibo_num.py
+++ py_lab/fibo_num.py
@@ -4,14 +4,16 @@ n = int(input("Fibonacci number: "))
 
 F0 = 0
 F1 = 1
-Fn = 0  # output
+Fn = 0	# output
 
-for i int range(1, n):
-  Fn = F0 + F1
-  F0 = F1
-  F1 = Fn
-  
-  print("%d" %Fn, end=' ')  # , 이건 어떻게 하지
-  
- print()
- print("F%d = %d" %(n, Fn))
+for i in range(1, n):
+	Fn = F0 + F1
+	
+	F0 = F1
+	F1 = Fn	 # go right
+
+	print("%d" %Fn, end=' ')  # , 이건 어떻게 하지
+
+print()
+
+print("F%d = %d " %(n, Fn))
